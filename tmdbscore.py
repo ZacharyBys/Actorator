4fad28352fce59656682210a331261a7cd7a3e6d
ZacharyBys <zachary.bys@gmail.com>
2018-08-21 19:06:53 -0400

    Initial commit with TMDb search for scores

---
+++ tmdbscore.py
@@ -0,0 +1,39 @@
+import tmdbsimple as tmdb
+import requests
+import json
+from privatekeys import *
+
+tmdb.API_KEY = tmdb_key
+
+def find_tmdb_score(movies):
+    voteTotal = 0
+    for movie in movies:
+        search = tmdb.Search()
+        response = search.movie(query=movie)
+        if search.results:
+            chosenMovie = search.results[0]
+            voteTotal = voteTotal + chosenMovie['vote_average']
+    return voteTotal/len(movies)
+
+def find_actor_id(actor):
+    search = tmdb.Search()
+    response = search.person(query=actor)
+    if search.results:
+        person = search.results[0]
+        return person['id']
+
+def find_actors_movies(actorId):
+    url = ('https://api.themoviedb.org/3/person/%s/movie_credits?api_key=%s&language=en-US' % (actorId, tmdb_key))
+    response = requests.get(url=url)
+    return json.loads(response.content)
+
+def find_score_by_person(person):
+    movies = find_actors_movies(find_actor_id(person))  
+    voteTotal = 0
+    if movies['cast']:
+        for movie in movies['cast']:
+            voteTotal = voteTotal + movie['vote_average']
+        return voteTotal/len(movies['cast'])
+    return None
+
+print(find_score_by_person('Cage'))
