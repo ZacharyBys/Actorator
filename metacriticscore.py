391ccd16bc62c22a53285c03bc8dbc7dda096ed6
ZacharyBys <zachary.bys@gmail.com>
2018-08-22 09:14:00 -0400

    Trying to grab metacritic score from google search stub

---
+++ metacriticscore.py
@@ -0,0 +1,6 @@
+from google import google
+#git+https://github.com/abenassi/Google-Search-API
+
+search_results = google.search('the dark knight rises metacritic')
+for result in search_results:
+    print result.description
