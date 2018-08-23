eece0c70949f568e1f7ee6d3b2e6d998e7ff8067
ZacharyBys <zachary.bys@gmail.com>
2018-08-23 11:28:30 -0400

    Can now find imdb, rotten tomatoes, and metacritic score from a movie title search

--- omdbscore.py
+++ omdbscore.py
@@ -1,14 +1,37 @@
 from privatekeys import omdb_key
 import requests
 import urllib
+import json
 
-baseUrl = 'http://www.omdbapi.com/?apikey=[yourkey]&'
-query = 'Batman v superman'
-params = {
-    'apiKey' : omdb_key,
-    's' : query
-}
+def findOmdbScoreFromId(imdbID):
+    baseUrl = 'http://www.omdbapi.com/'
+    params = {
+        'apiKey' : omdb_key,
+        'i' : imdbID
+    }
 
-url = baseUrl + '?' + urllib.parse.urlencode(params)
-movieInfo = requests.get(url).content
-print(movieInfo)
+    url = baseUrl + '?' + urllib.urlencode(params)
+    result = json.loads(requests.get(url).content)
+
+    results = {}
+    if result['Ratings']:
+        for rating in result['Ratings']:
+            results[rating['Source']] = rating['Value']
+
+    return(results)
+
+def findOmdbScore(movie):
+    baseUrl = 'http://www.omdbapi.com/'
+    params = {
+        'apiKey' : omdb_key,
+        's' : movie
+    }
+
+    url = baseUrl + '?' + urllib.parse.urlencode(params)
+    searchResults = json.loads(requests.get(url).content)
+    if searchResults['Search'][0]:
+        return findOmdbScoreFromId(searchResults['Search'][0]['imdbID'])
+    else:
+        return -1
+
+print(findOmdbScore('Batman v superman'))
