59455361b1a1b71e24b8d6d719571b39e03d9aae
Anthony Laye <anthony_laye@hotmail.com>
2018-08-22 20:26:11 -0400

    Scores from rottentomatoes

--- rottentomatoes.py
+++ rottentomatoes.py
@@ -1,3 +1,18 @@
-def test:
-	pass
-	
+from bs4 import BeautifulSoup
+import requests
+from urllib.request import urlopen
+
+movie = 'slender_man'
+url = 'http://www.rottentomatoes.com/m/{}'.format(movie)
+
+page = urlopen(url)
+soup = BeautifulSoup(page, 'html.parser')
+
+criticscore = soup.find('div', attrs={'class': 'critic-score meter'}).find('span', attrs={'class':'superPageFontColor'})
+communityscore = soup.find('div', attrs={'class': 'audience-score meter'}).find('span', attrs={'class':'superPageFontColor'})
+
+print('{}: Tomato score: {}'.format(movie, criticscore.text.strip()))
+print('{}: Audience score: {}'.format(movie, communityscore.text.strip()))
+
+if int(communityscore.text.strip('%')) < 30:
+	print('Ouch! Tough crowd!')
