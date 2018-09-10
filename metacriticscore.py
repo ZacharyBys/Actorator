947d7c3af2e44426546f38bd4d3909a3e5c81ef6
Anthony Laye <anthony_laye@hotmail.com>
2018-09-09 21:06:57 -0400

    Delete old files

--- metacriticscore.py
+++
@@ -1,6 +0,0 @@
-from google import google
-#git+https://github.com/abenassi/Google-Search-API
-
-search_results = google.search('the dark knight rises metacritic')
-for result in search_results:
-    print(result.description)
